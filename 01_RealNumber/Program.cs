c88cad1003f9c577921bb893bdf14becaa26a5ae
Mira <5555159m@gmail.com>
2022-06-06 22:16:22 +0300

    add 1 task

---
+++ 01_RealNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами
+
+double [,] Matrix = new double [10,10];
+{
+    for (int i = 0; i < Matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < Matrix.GetLength(1); j++)
+        {
+            Matrix[i,j] = Math.Round((new Random().NextDouble()) * 10,2);
+            Console.Write(Matrix[i,j] + " ");
+        }
+    Console.WriteLine();
+    }
+}
